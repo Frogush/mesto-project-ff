248b0e73ad4b0b0f7a019a896c66527b750b23dd
Frogush <aes-09k@mail.ru>
2025-03-04 23:25:47 +0300

    v1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,68 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(name, link) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardTitle.textContent = name;
+
+  deleteButton.addEventListener("click", () => {
+    deleteCard(deleteButton);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(button) {
+  button.closest(".card").remove();
+}
 
 // @todo: Вывести карточки на страницу
+function renderCards(cards) {
+  cards.forEach((card) => {
+    const cardElement = createCard(card.name, card.link);
+    placesList.appendChild(cardElement);
+  });
+}
+
+renderCards(initialCards);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
