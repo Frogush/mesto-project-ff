22bcbeebf16066e373d7416b72cf78ccda39b236
Frogush <aes-09k@mail.ru>
2025-03-29 12:38:37 +0300

    v1.0.1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,32 +6,32 @@ import { openPopUp, closePopUp } from "../components/modal";
 // @todo: DOM узлы
 
 // DOM узлы: Кнопки
-const profileEditButton = document.querySelector(".profile__edit-button");
-const profileAddButton = document.querySelector(".profile__add-button");
+const editProfileButton = document.querySelector(".profile__edit-button");
+const addProfileButton = document.querySelector(".profile__add-button");
 
 // DOM узлы: Все PopUp's
-const popUps = document.querySelectorAll(".popup");
+const popups = document.querySelectorAll(".popup");
 
 // DOM узлы: Для карточек
 const cardsContainer = document.querySelector(".places__list");
-const popupTypeNewCard = document.querySelector(".popup_type_new-card");
-const formElementTypeNewCard = popupTypeNewCard.querySelector(".popup__form");
-const namePlace = formElementTypeNewCard.querySelector(".popup__input_type_card-name");
-const urlCardImage = formElementTypeNewCard.querySelector(".popup__input_type_url");
+const popupNewCard = document.querySelector(".popup_type_new-card");
+const formNewCard = popupNewCard.querySelector(".popup__form");
+const inputCardName = formNewCard.querySelector(".popup__input_type_card-name");
+const inputCardUrl = formNewCard.querySelector(".popup__input_type_url");
 
 // DOM узлы: Для редактирования профиля
-const popupTypeEdit = document.querySelector(".popup_type_edit");
-const formElementTypeEdit = popupTypeEdit.querySelector(".popup__form");
-const nameInput = formElementTypeEdit.querySelector(".popup__input_type_name");
-const jobInput = formElementTypeEdit.querySelector(".popup__input_type_description");
-const profileTitle = document.querySelector(".profile__title");
-const profileDescription = document.querySelector(".profile__description");
+const popupEditProfile = document.querySelector(".popup_type_edit");
+const formEditProfile = popupEditProfile.querySelector(".popup__form");
+const inputName = formEditProfile.querySelector(".popup__input_type_name");
+const inputDescription = formEditProfile.querySelector(".popup__input_type_description");
+const profileName = document.querySelector(".profile__title");
+const profileBio = document.querySelector(".profile__description");
 
 // @todo: Выводит все карточки из массива initialCards файла cards.js
 renderCards(initialCards);
 
 // @todo: Функций закрытия popup через крестик для всех popup
-popUps.forEach((popup) => {
+popups.forEach((popup) => {
     const closeButton = popup.querySelector(".popup__close");
   
     closeButton.addEventListener("click", () => closePopUp(popup));
@@ -50,46 +50,46 @@ function addNewCard(evt) {
     evt.preventDefault();
   
     const cardData = {
-      name: namePlace.value,
-      link: urlCardImage.value,
+      name: inputCardName.value,
+      link: inputCardUrl.value,
     };
   
     const card = createCard(cardData.name, cardData.link, deleteCard, likeCard, openPopUpImage);
   
     cardsContainer.prepend(card);
-    closePopUp(popupTypeNewCard);
+    closePopUp(popupNewCard);
   
-    formElementTypeNewCard.reset();
+    formNewCard.reset();
 }
 
 // @todo: Функция редактирования профиля
 function addNewProfile(evt) {
     evt.preventDefault();
   
-    profileTitle.textContent = nameInput.value;
-    profileDescription.textContent = jobInput.value;
+    profileName.textContent = inputName.value;
+    profileBio.textContent = inputDescription.value;
   
-    closePopUp(popupTypeEdit);
+    closePopUp(popupEditProfile);
   
-    formElementTypeEdit.reset();
+    formEditProfile.reset();
 }
 
 // @todo: Функция заполнения полей профиля перед открытием попапа
 function addProfileValues() {
-    nameInput.value = profileTitle.textContent;
-    jobInput.value = profileDescription.textContent;
+    inputName.value = profileName.textContent;
+    inputDescription.value = profileBio.textContent;
 }
 
 // @todo: Добавляем обработчики событий для открытия попапов
-profileEditButton.addEventListener("click", () => {
-    openPopUp(popupTypeEdit);
+editProfileButton.addEventListener("click", () => {
+    openPopUp(popupEditProfile);
     addProfileValues();
 });
 
-profileAddButton.addEventListener("click", () =>
-    openPopUp(popupTypeNewCard)
+addProfileButton.addEventListener("click", () =>
+    openPopUp(popupNewCard)
 );
 
 // @todo: Добавляем обработчики событий для форм
-formElementTypeEdit.addEventListener("submit", addNewProfile);
-formElementTypeNewCard.addEventListener("submit", addNewCard);
+formEditProfile.addEventListener("submit", addNewProfile);
+formNewCard.addEventListener("submit", addNewCard);
